d6ba48fffef8111693849a844d054fc4eef1e793
vishwas <vishwas452003@gmail.com>
2024-02-08 22:36:29 +0530

    first commit frontend

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Budget Buddy</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="log-container">
+        <div class="logo-div">
+            <img src="./images/logo.png" alt="" class="img" />
+        </div>
+        <div class="head">
+            <p>Budget Buddy</p>
+        </div>
+        <div class="log-head">
+            <p>Login</p>
+        </div>
+        <form action="welcome.php">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="submit" value="Login" class="login-bt" />
+        </form>
+    </div>
+    <!--log-container-->
+</body>
+
+</html>
