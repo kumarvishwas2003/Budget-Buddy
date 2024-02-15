17efcfc42ea835aad17ffc566dfe2f0bb68f6e44
vishwas <vishwas452003@gmail.com>
2024-02-15 12:09:36 +0530

    dashboard

--- login.php
+++ login.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
+
+include 'connection.php';
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM login where username='$username' && password = '$password' ";
+    $data = mysqli_query($conn, $sql);
+    $total = mysqli_num_rows($data);
+    if ($total == 1) {
+        // echo "matched";
+        $_SESSION['username'] = $username;
+
+        header("location:welcome.php");
+    } else {
+        $failed = 'Log in failed';
+        // echo $failed;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +26,11 @@ session_start();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Budget Buddy</title>
-    <link rel="stylesheet" href="./CSS/style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />
+    <link rel=" preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="CSS/style.css" />
 </head>
 
 
@@ -28,8 +46,12 @@ session_start();
             <p>Login</p>
         </div>
         <form action="#" method="POST">
-            <input type="text" placeholder="Username" name="username" />
-            <input type="password" placeholder="Password" name="password" />
+            <input type="text" placeholder="Username" name="username" required />
+            <input type="password" placeholder="Password" name="password" required />
+
+            <?php if (isset($failed)) { ?>
+                <div class="error"><?php echo $failed; ?></div>
+            <?php } ?>
             <input type="submit" value="Login" class="login-bt" name="login" />
         </form>
     </div>
@@ -37,23 +59,3 @@ session_start();
 </body>
 
 </html>
-
-<?php
-include 'connection.php';
-if (isset($_POST['login'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM login where username='$username' && password = '$password' ";
-    $data = mysqli_query($conn, $sql);
-    $total = mysqli_num_rows($data);
-    if ($total == 1) {
-        // echo "matched";
-        $_SESSION['username'] = $username;
-
-        header("location:welcome.php");
-    } else {
-        echo "failed";
-    }
-}
-
-?>
--- welcome.php
+++ welcome.php
@@ -11,6 +11,9 @@ session_start();
     <title>Welcome to Budget Buddy</title>
     <link rel="stylesheet" href="./CSS/welcome.css">
     <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 
 </head>
 
@@ -21,9 +24,9 @@ session_start();
     } else {
         header('location:login.php');
     }
-    echo "hello " . $user_profile;
+    // echo "hello " . $user_profile; //thsi has to be used in welcome message using isset
     ?>
-    <div class="welcome-container">
+    <!-- <div class="welcome-container">
         <div class="logo-div">
             <img src="./images/logo.png" alt="logo" class="img">
         </div>
@@ -36,8 +39,39 @@ session_start();
             journey and achieve your goals with confidence and ease. Let's start budgeting together!
         </div>
         <input type="button" value="Next">
-        <a href="logout.php"><input type="submit" value="logout"></a>
+        
+    </div> -->
+    <div class="welcome-container">
+        <div>
+            <img src="./images/logo.png" class="bg-logo">
+        </div>
+        <div class="log-out-div">
+            <a href="logout.php"><input type="submit" value="Logout" class="log-out-btn"></a>
+        </div>
+        <div class="welcome-head">
+            <h1>Welcome <?php echo $user_profile ?></h1>
+        </div>
+        <div class="content-head">
+            <p>Happy Budgeting! </p>
+        </div>
+        <div class="content-para">
+            <p>
+
+                We're delighted to welcome you to Budget Buddy, your go-to tool for managing your finances with ease.
+                Here, you can effortlessly track your expenses, set budget goals, and gain valuable insights into your
+                spending habits.
+                <br>
+                Start exploring Budget Buddy now to take control of your finances and achieve your financial goals.
+                <br>
+                <br>
+                The Budget Buddy Team"
+            </p>
+        </div>
+        <div class="next-btn-div"><input type="button" value="Next" class="next-btn"></a></div>
     </div>
+    <!--welcome-container-->
+
+
 </body>
 
 </html>
